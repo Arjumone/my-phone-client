d56d33d8e04f5f07ce074cf4ca9e2b5134253f55
Arju Mone <121635205+Arjumone@users.noreply.github.com>
2023-10-11 12:39:58 +0600

    added server

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './components/Main.jsx';
+import Phones from './components/Phones.jsx';
+import Phone from './components/Phone';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/phones",
+        element: <Phones></Phones>,
+        loader:()=>fetch("")
+      },
+      {
+        path:"/phone/:id",
+        element:<Phone></Phone>,
+        loader:({params})=>fetch(``)
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
